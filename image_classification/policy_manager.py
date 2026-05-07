f5dd51c3276c69ea06e3c480f2164100e568c270
MrPsarik <mrpsarik@gmail.com>
2026-05-07 16:39:31 +0200

    fix(image_classification): solved path to the script resolving issue (need testing on hardware)

--- image_classification/policy_manager.py
+++ image_classification/policy_manager.py
@@ -3,25 +3,25 @@ import uuid
 from pathlib import Path
 
 
-ROBOT_PORT = "/dev/ttyACM0"  # Windows example. Linux example: "/dev/ttyACM0"
+ROBOT_PORT = "/dev/ttyACM0"
 ROBOT_ID = "my_so101_follower"
 
-CAMERA_CONFIG = "{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30}}"
+CAMERA_CONFIG = "{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30}, top: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30}}"
 
 POLICIES = {
     0: {
         "name": "recycle",
-        "policy_path": "YOUR_HF_USERNAME/recycle_act_policy",
+        "policy_path": "YOUR_HF_USERNAME/recycle_act_policy", # Ommited for now
         "task": "Put the recyclable object in the recycle bin",
     },
     1: {
         "name": "scrap_paper",
-        "policy_path": "MrPsarik/movement1",
+        "policy_path": "MrPsarik/movement1", # Dummy movement 1
         "task": "Put the scrap paper in the paper bin",
     },
     2: {
         "name": "toy",
-        "policy_path": "MrPsarik/movement2",
+        "policy_path": "MrPsarik/movement2", # Dummt movement 2
         "task": "Put the toy in the toy bin",
     },
 }
@@ -39,10 +39,10 @@ def run_act_policy_blocking(class_id: int):
 
     policy = POLICIES[class_id]
 
-    eval_repo_id = f"local/eval_{policy['name']}_{uuid.uuid4().hex[:8]}"
+    LEROBOT_REPLAY = Path(sys.executable).parent / "lerobot-replay"
 
     command = [
-        "/home/mrpsarik/miniforge3/envs/lerobot/bin/lerobot-replay", 
+        str(LEROBOT_REPLAY), 
         "--robot.type=so101_follower",
         "--robot.port=/dev/ttyACM0",
         "--robot.id=follower_arm",
